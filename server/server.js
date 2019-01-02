f91dd1174e985c57d39bac9ae80678bbbae6f18c
Elagrebi Ali <elagrebi.ali0gmail.com>
2019-01-02 12:45:28 +0100

    Adding socket and config a server for web sockets

--- server/server.js
+++ server/server.js
@@ -1,17 +1,34 @@
 require('./config/config')
 
+/*built in module*/
 const path=require('path')
-const express=require('express')
+const http=require('http')
 
+/*install module*/
+const express=require('express')
+const socketIO=require('socket.io')
 
 const publicPath=path.join(__dirname,'../public')
 const port=process.env.PORT
-console.log(port)
+
 var app=express()
+var server =http.createServer(app)
+/*pass the server that we want to use with our web sockets(integrate socket with our server) and have acess to javascript library*/
+/*with io we can emitting or listening to events , to got communicate between the server
+and the client  */
+var io=socketIO(server)
 
 app.use(express.static(publicPath))
+/*using io.on() let's you register an event lessner */
+io.on('connection',(socket)=>{
+  /*this socket argument we have access to over inside an index.html*/
+  console.log('New user connected')
+  socket.on('disconnect',()=>{
+    console.log('User is disconnected')
+  })
+})
 
 
-app.listen(port,()=>{
+server.listen(port,()=>{
   console.log(`Server is up on ${port}`)
 })
