da53fe9ca572584ea9a2478e9f420730952ed9b4
Elagrebi Ali <elagrebi.ali0gmail.com>
2019-01-03 19:23:44 +0100

    auto-scrolling using jQuery .prop()

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,27 @@
   /*have the live connection between the client and the server via io method in javascript library*/
 var socket=io()
+
+/*autoscrolling functio*/
+function autoScrolling(){
+  //select DOM
+  var messages=jQuery('#messages')
+  var message=jQuery('#messages li:last-child')
+
+  //get Height of the selector
+  var scrollHeight=messages.prop('scrollHeight'),
+      scrollTop=messages.prop('scrollTop'),
+      clientHeight=messages.prop('clientHeight'),
+      /*This method returns the height of the element, including top and bottom padding*/
+      newMessage=message.innerHeight(),
+      /*To select the element that comes immediately before the newMessage*/
+      lastMessage=message.prev().innerHeight()
+      console.log(`scrollHeight:${scrollHeight},scrollTop:${scrollTop},clientHeight:${clientHeight},newMessage:${newMessage}`)
+if(clientHeight+scrollTop+newMessage+lastMessage>=scrollHeight){
+  /*setting the scrollTop value with .scrollTop method in jquery*/
+  messages.scrollTop(scrollHeight)
+}
+}
+
 socket.on('connect',()=>{
   console.log('Connected to server')
 
@@ -21,7 +43,7 @@ socket.on('newMessage',(message)=>{
   // var li=jQuery('<li></li>')
   // li.text(`${message.from} ${timeMoment}:${message.text}`)
   jQuery('#messages').append(html)
-
+  autoScrolling()
 })
 
 socket.on('newLocation',(location)=>{
@@ -40,6 +62,7 @@ socket.on('newLocation',(location)=>{
   // a.attr('href',location.url)
   // li.append(a)
   jQuery('#messages').append(html)
+  autoScrolling()
 })
 
 /*attached the event with jquery*/
